a4926848f0998a3b7e2acf36c38b1e36b5dacea8
JhonatanClearminds <jhonatanabrigo.clearminds@gmail.com>
2026-07-14 16:35:32 -0500

    fix: correcion de link invita y gana

--- reybingo.com/public_html/app/Views/users/referrals.php
+++ reybingo.com/public_html/app/Views/users/referrals.php
@@ -18,9 +18,7 @@
                     </small>
 
                     <div class="mt-2 p-2 rounded" style="background: rgba(98,54,255,0.12);">
-                        <strong><?= translate('point of sale'); ?></strong>
-                        <p class="small mb-1"><?= translate('point of sale link share help'); ?></p>
-                        <input type="text" class="form-control form-control-sm form-bingo mb-1" id="affiliate-link" readonly value="<?= site_url('signup/' . ($user['referred_code'] ?? $user['code'])); ?>">
+                        <input type="text" class="form-control form-control-sm form-bingo mb-2" id="affiliate-link" readonly value="<?= site_url('signup/' . ($user['referred_code'] ?? $user['code'])); ?>">
                         <button type="button" class="btn btn-primary btn-bingo btn-sm" onclick="navigator.clipboard.writeText(document.getElementById('affiliate-link').value);">Copiar enlace</button>
                     </div>
         
